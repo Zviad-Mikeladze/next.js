6bc68d48eaa97f0ed8abedc11ea47be42d1bde33
Zviad Mikeladze <zviadipf@gmail.com>
2022-10-17 19:03:56 +0400

    Final commit

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/somethin-important
 
 function DetailPage() {
+
+  const router= useRouter();
+ const newsId= router.query.newsid
+
   return <h1>The Detail Page</h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 // our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
 
-function NewsPage () {
-    return <h1>The News Page</h1>
+function NewsPage() {
+  return ( 
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">Nextjs</Link>
+        </li>
+        <li>Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
-export default NewsPage
+export default NewsPage;
